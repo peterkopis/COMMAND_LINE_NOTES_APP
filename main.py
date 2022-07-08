5579ebb0c6c14efd3ebec6de093a0adc0a6b128b
Peter <peter.pis@centrum.sk>
2022-07-08 18:04:48 +0200

    add the repetition if user not write correct answer

--- main.py
+++ main.py
@@ -2,4 +2,12 @@
 from datetime import datetime
 
 now= datetime.now().strftime("%d/%m/%Y")
-print(f"\nWelcome in notes app, today is {now} \n\n -you can create an acount (create)\n -or login in your existing acount (login)\n")
+
+while(True):
+    print(f"\nWelcome in notes app, today is {now} \n\n -you can create an acount (create)\n -or login in your existing acount (login)\n")
+    first_action =input().lower()
+    print(first_action)
+    if (first_action == "create" or first_action == "login"):
+        break
+    #if first_action == create:
+
